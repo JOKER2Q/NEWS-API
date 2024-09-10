ccd0a4a6a74154f0d3b43744c562a6b8099504df
JOKER2Q <rinas1122k@gmail.com>
2024-09-10 01:42:07 -0700

    added users

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,20 +1,20 @@
 const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/userController");
-
+const authenticateToken = require("../middleware/authMiddleware");
 // Route to get all users
 router.route("/").get(userController.getAllUsers);
 
 // Route to create a new user
-router.route("/").post(userController.createUser);
+router.route("/").post(authenticateToken, userController.createUser);
 
 // Route to get a specific user by ID
 router.route("/:id").get(userController.getUserById);
 
 // Route to update a specific user by ID
-router.route("/:id").put(userController.updateUserById);
+router.route("/:id").put(authenticateToken, userController.updateUserById);
 
 // Route to delete a specific user by ID
-router.route("/:id").delete(userController.deleteUserById);
+router.route("/:id").delete(authenticateToken, userController.deleteUserById);
 
 module.exports = router;
